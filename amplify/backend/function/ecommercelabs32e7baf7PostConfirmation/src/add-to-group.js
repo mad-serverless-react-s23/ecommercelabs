ad4cc429913c0d3ebaf932ceef7be28c8cfcac50
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-05-02 18:20:41 -0500

    Update add-to-group.js
    manually entered the data to fuck with myself. idk c/p seemed to be a problem last time

--- amplify/backend/function/ecommercelabs32e7baf7PostConfirmation/src/add-to-group.js
+++ amplify/backend/function/ecommercelabs32e7baf7PostConfirmation/src/add-to-group.js
@@ -1,8 +1,46 @@
 const aws = require('aws-sdk');
 
-const cognitoIdentityServiceProvider = new aws.CognitoIdentityServiceProvider({
-  apiVersion: '2016-04-18',
-});
+exports.handler = async (event, context, callback) => {
+  const cognitoProvider = new aws.CognitoIdentityServiceProvider({
+    apiVersion: '2016-04-18',
+  });
+
+  let isAdmin = false;
+
+  const adminEmails = ['ccrawford1@madisoncollege.edu', 'denecrux@gmail.com'];
+
+  if (adminEmails.indexOf(event.request.userAttributes.email) !== -1) {
+    isAdmin = true;
+  };
+
+  if (isAdmin) {
+    const groupParams = {
+      UserPoolId: event.userPoolId,
+      GroupName: 'Admin'
+    }
+    const userParams = {
+      UserPoolId: event.userPoolId,
+      Username: event.userName,
+      GroupName: 'Admin'
+    }
+    // if group not present, create it
+    try {
+      await cognitoProvider.getGroup(groupParams).promise();
+    } catch (e) {
+      await cognitoProvider.createGroup(groupParams).promise();
+    }
+
+    try {
+      await cognitoProvider.adminAddUserToGroup(userParams).promise();
+      callback(null, event);
+    } catch (e) { callback(e); }
+  } else {
+    // user in neither group
+    callback(null, event);
+  }
+};
+
+
 
 /**
  * @type {import('@types/aws-lambda').PostConfirmationTriggerHandler}
