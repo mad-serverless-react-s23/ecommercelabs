c230ccb610f2c9541cdcff214b0bea9cbbdd9623
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-05-03 02:17:10 -0500

    Update Profile.js
    there ya happy?

---
+++ src/Profile.js
@@ -0,0 +1,28 @@
+import './App.css';
+import { Authenticator } from '@aws-amplify/ui-react';
+import { Button } from 'antd';
+import '@aws-amplify/ui-react/styles.css';
+
+const Profile = () => {
+    return (
+        <div style={containerStyle}>
+            <Authenticator>
+            {({ signOut, user }) => (
+                <>
+                <h1>Content!</h1>
+                <h2>This is your profile, I guess</h2>
+                <h3>{user.username}</h3>
+                <Button onClick={signOut}>Click to sign out?</Button>
+                </>
+            )}
+            </Authenticator>
+        </div>
+    )
+}
+
+const containerStyle = {
+    width: 400,
+    margin: '20px auto'
+}
+
+export default Profile
